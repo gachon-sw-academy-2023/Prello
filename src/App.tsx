849cdb40d4702b3e54b11865d38c5018d0bbfed2
seyiul <iseuli713@gmail.com>
2023-01-16 11:45:18 +0900

    feat: 회원가입 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/authorization/login';
+import SignUp from './pages/authorization/sign-up';
 import routes from './routes';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path={routes.LOGIN} element={<Login />} />
+        <Route path={routes.SIGNUP} element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
