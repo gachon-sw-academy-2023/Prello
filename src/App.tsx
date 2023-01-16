fe0d10c4ee7a59e531b201cead6cc77caeba9725
seyiul <iseuli713@gmail.com>
2023-01-16 09:50:01 +0900

    feat: 라우팅 네이밍 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path={routes.LOGIN} element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
