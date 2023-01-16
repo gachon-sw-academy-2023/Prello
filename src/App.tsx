3c4642c4561987f530ec6369a7f12a9b5744fd4a
seyiul <iseuli713@gmail.com>
2023-01-16 11:47:40 +0900

    Revert "feat: 회원가입 UI 구현"
    This reverts commit 849cdb40d4702b3e54b11865d38c5018d0bbfed2.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/authorization/login';
-import SignUp from './pages/authorization/sign-up';
 import routes from './routes';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path={routes.LOGIN} element={<Login />} />
-        <Route path={routes.SIGNUP} element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
