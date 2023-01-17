a1ecfc577b2e9ce752f29086f963893f129e9af4
SeonDahye <adad05011@gachon.ac.kr>
2023-01-17 12:03:36 +0900

    style: min-height 설정

--- src/pages/workspace/default/styles.ts
+++ src/pages/workspace/default/styles.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 // 전체 div
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   background-color: #f5f6f7;
   padding: 0 50px;
 `;
