f08fbaa76c1d1cd62769f1690c86952835fcbe31
SeonDahye <adad05011@gachon.ac.kr>
2023-01-19 14:33:57 +0900

    refactor: import 정리

--- src/pages/workspace/detail/index.tsx
+++ src/pages/workspace/detail/index.tsx
@@ -1,31 +1,9 @@
-import styled from '@emotion/styled';
-import Grid from '@mui/material/Grid';
-import {
-  Container,
-  Line,
-  LeftContainer,
-  LeftContent,
-  LeftContentNotHover,
-  Title,
-  MembersWrapper,
-  Image,
-  ProfileName,
-  RightContainer,
-  InfoContainer,
-  WorkspaceImage,
-  InfoContents,
-  ExplainText,
-  BoardContainer,
-  Item,
-  Icon,
-  MemberWrapper,
-  Wrapper,
-} from './styles';
-import { BarInfo, BarImg, WorkspaceName } from '@/pages/board/styles';
-import WorkspaceImg from '@/components/WorkspaceImg/WorkspaceImg';
 import SideBar from '@/components/SideBar/SideBar';
-import { SubTitle } from '@/components/SubTitle/SubTitle.styles';
 import { SubHeader } from '@/components/SubHeader/SubHeader';
+import { SubTitle } from '@/components/SubTitle/SubTitle.styles';
+import WorkspaceImg from '@/components/WorkspaceImg/WorkspaceImg';
+import Grid from '@mui/material/Grid';
+import * as S from './styles';
 
 export default function WorkspaceDetail() {
   interface IMember {
@@ -91,44 +69,44 @@ export default function WorkspaceDetail() {
   ];
 
   return (
-    <Container>
+    <S.Container>
       <SubHeader />
-      <Wrapper>
+      <S.Wrapper>
         <SideBar memberInfo={members} />
-        <RightContainer>
-          <InfoContainer>
+        <S.RightContainer>
+          <S.InfoContainer>
             <WorkspaceImg
               radius="none"
               image="/assets/authorization/pimfy_profile.png"
             />
-            <InfoContents>
+            <S.InfoContents>
               <SubTitle size="md">PIMPY</SubTitle>
-              <ExplainText>핌피팀입니당</ExplainText>
-            </InfoContents>
-          </InfoContainer>
-          <Line margin="0"></Line>
-          <BoardContainer>
+              <S.ExplainText>핌피팀입니당</S.ExplainText>
+            </S.InfoContents>
+          </S.InfoContainer>
+          <S.Line margin="0"></S.Line>
+          <S.BoardContainer>
             <Grid container spacing={4}>
               {boards.map((board) => (
                 <Grid item xs={12} sm={6} md={4} key={board.title}>
-                  <Item center={false} color={'#ffe7ee'}>
-                    <Title margin={'30px 20px'}>{board.title}</Title>
-                  </Item>
+                  <S.Item center={false} color={'#ffe7ee'}>
+                    <S.Title margin={'30px 20px'}>{board.title}</S.Title>
+                  </S.Item>
                 </Grid>
               ))}
               <Grid item xs={12} sm={6} md={4}>
-                <Item center={true} color={'#fffcff'}>
-                  <Image
+                <S.Item center={true} color={'#fffcff'}>
+                  <S.Image
                     width={'50px'}
                     height={'50px'}
                     img={'/assets/workspace/sample-add-icon.png'}
-                  ></Image>
-                </Item>
+                  ></S.Image>
+                </S.Item>
               </Grid>
             </Grid>
-          </BoardContainer>
-        </RightContainer>
-      </Wrapper>
-    </Container>
+          </S.BoardContainer>
+        </S.RightContainer>
+      </S.Wrapper>
+    </S.Container>
   );
 }
