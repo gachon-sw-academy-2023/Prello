258799244a15ac10094a9e3f2f2eae6999bc5934
SeonDahye <adad05011@gachon.ac.kr>
2023-01-19 11:33:51 +0900

    feat: 워크스페이스 페이지에 헤더 추가

--- src/pages/workspace/default/index.tsx
+++ src/pages/workspace/default/index.tsx
@@ -1,56 +1,70 @@
+import Button from '@/components/Button/Button';
+import ProfileImg from '@/components/ProfileImg/ProfileImg';
+import { SubHeader } from '@/components/SubHeader/SubHeader';
+import { Default, Mobile } from '@/utils/mediaQuery';
 import Grid from '@mui/material/Grid';
 import { useNavigate } from 'react-router-dom';
 import * as S from './styles';
-import Button from '@/components/Button/Button';
-import ProfileImg from '@/components/ProfileImg/ProfileImg';
-import { Mobile, Default } from '@/utils/mediaQuery';
 export default function WorkspaceDefault() {
   const navigate = useNavigate();
   return (
     <S.Container>
-      <S.Wrapper>
-        <S.Title>나의 워크스페이스</S.Title>
-        <Mobile>
-          <Button width={20} radius={'rounded'} color={'primary'} shadow={true}>
-            + 새로운 워크스페이스
-          </Button>
-        </Mobile>
-        <Default>
-          <Button width={50} radius={'rounded'} color={'primary'} shadow={true}>
-            + 새로운 워크스페이스
-          </Button>
-        </Default>
-      </S.Wrapper>
+      <SubHeader />
+      <S.ContentsWrapper>
+        <S.Wrapper>
+          <S.Title>나의 워크스페이스</S.Title>
+          <Mobile>
+            <Button
+              width={20}
+              radius={'rounded'}
+              color={'primary'}
+              shadow={true}
+            >
+              + 새로운 워크스페이스
+            </Button>
+          </Mobile>
+          <Default>
+            <Button
+              width={50}
+              radius={'rounded'}
+              color={'primary'}
+              shadow={true}
+            >
+              + 새로운 워크스페이스
+            </Button>
+          </Default>
+        </S.Wrapper>
 
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={4} md={3}>
-          <S.Item
-            onClick={() => {
-              navigate('/workspace-detail');
-            }}
-          >
-            <S.GradientBG></S.GradientBG>
-            <S.ItemContents>
-              <S.Title>MOKA</S.Title>
-              <S.ItemBoardName>First Board</S.ItemBoardName>
-              <S.ProfileImages>
-                <ProfileImg image="/assets/workspace/sample-profile-image.png" />
-                <ProfileImg image="/assets/workspace/sample-profile-image.png" />
-                <ProfileImg image="/assets/workspace/sample-profile-image.png" />
-              </S.ProfileImages>
-            </S.ItemContents>
-          </S.Item>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4} md={3}>
+            <S.Item
+              onClick={() => {
+                navigate('/workspace-detail');
+              }}
+            >
+              <S.GradientBG></S.GradientBG>
+              <S.ItemContents>
+                <S.Title>MOKA</S.Title>
+                <S.ItemBoardName>First Board</S.ItemBoardName>
+                <S.ProfileImages>
+                  <ProfileImg image="/assets/workspace/sample-profile-image.png" />
+                  <ProfileImg image="/assets/workspace/sample-profile-image.png" />
+                  <ProfileImg image="/assets/workspace/sample-profile-image.png" />
+                </S.ProfileImages>
+              </S.ItemContents>
+            </S.Item>
+          </Grid>
+          <Grid item xs={12} sm={4} md={3}>
+            <S.Item></S.Item>
+          </Grid>
+          <Grid item xs={12} sm={4} md={3}>
+            <S.Item></S.Item>
+          </Grid>
+          <Grid item xs={12} sm={4} md={3}>
+            <S.Item></S.Item>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <S.Item></S.Item>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <S.Item></S.Item>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <S.Item></S.Item>
-        </Grid>
-      </Grid>
+      </S.ContentsWrapper>
     </S.Container>
   );
 }
--- src/pages/workspace/default/styles.ts
+++ src/pages/workspace/default/styles.ts
@@ -1,26 +1,30 @@
 import styled from '@emotion/styled';
 // 전체 div
-const Container = styled.div`
+export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
+  background-color: #f5f6f7;
+`;
+export const ContentsWrapper = styled.div`
+  width: 100%;
   background-color: #f5f6f7;
   padding: 0 50px;
 `;
 
 // 타이틀 + 버튼 감싸는 div
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 20px 0;
 `;
-const Title = styled.h1`
+export const Title = styled.h1`
   font-size: 22px;
   font-weight: 600;
   font-family: 'LINESeedKR-Bd';
   color: #4f4e4e;
 `;
-const CreateButton = styled.button`
+export const CreateButton = styled.button`
   font-family: 'LINESeedKR-Rg';
   background-color: #fca4be;
   color: white;
@@ -32,7 +36,7 @@ const CreateButton = styled.button`
 `;
 
 // 워크스페이스 아이템
-const Item = styled.div`
+export const Item = styled.div`
   display: flex;
   background-color: white;
   width: 100%;
@@ -42,46 +46,33 @@ const Item = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
 `;
 // 아이템 왼쪽 그라데이션
-const GradientBG = styled.div`
+export const GradientBG = styled.div`
   width: 60px;
   height: 200px;
   border-top-left-radius: 0.5rem;
   border-bottom-left-radius: 0.5rem;
   background: linear-gradient(to bottom, #fca4be 0%, #e5a4fc 100%);
 `;
-const ItemContents = styled.div`
+export const ItemContents = styled.div`
   width: 240px;
   height: 200px;
   padding: 30px 10px;
 `;
-const ItemBoardName = styled.h1`
+export const ItemBoardName = styled.h1`
   padding: 20px 0 50px 0;
   font-size: 18px;
   font-weight: 500;
   font-family: 'LINESeedKR-Rg';
   color: #4f4e4e;
 `;
-const ProfileImages = styled.div`
+export const ProfileImages = styled.div`
   display: flex;
   justify-content: flex-start;
 `;
-const ProfileImage = styled.div`
+export const ProfileImage = styled.div`
   width: 40px;
   height: 40px;
   margin-right: 10px;
   background-size: cover;
   background-image: url('/assets/workspace/sample-profile-image.png');
 `;
-
-export {
-  Container,
-  Wrapper,
-  Title,
-  CreateButton,
-  Item,
-  GradientBG,
-  ItemContents,
-  ItemBoardName,
-  ProfileImages,
-  ProfileImage,
-};
