c5d435580c170322e7ccc88fad36365e538e80ff
seyiul <iseuli713@gmail.com>
2023-02-06 11:05:24 +0900

    fix: 아이템 draggable 설정

--- src/pages/board/Item/Item.tsx
+++ src/pages/board/Item/Item.tsx
@@ -14,7 +14,7 @@ const Item: React.FC<IItemProps> = ({ children, itemId }) => {
     console.log(isOpen);
   };
   return (
-    <S.ItemContainer id={`${itemId}`}>
+    <S.ItemContainer id={`${itemId}`} draggable="true">
       <S.Item onClick={() => setOpen(true)}>{children}</S.Item>
       {isOpen && <Detail setOpen={handleModal} />}
     </S.ItemContainer>
