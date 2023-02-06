03f7b4261e8fb415ed82d925abb5b26b858d063a
seyiul <iseuli713@gmail.com>
2023-02-06 15:31:12 +0900

    fix: restore boardHandler.ts

--- src/mocks/browsers.ts
+++ src/mocks/browsers.ts
@@ -3,9 +3,11 @@ import { handlers } from './handlers';
 import { workspaceHandlers } from './workspaceHandlers';
 import { itemHandlers } from './Board/itemHandlers';
 import { cardHandlers } from './Board/cardHandlers';
+import { boardHandlers } from './boardHandlers';
 export const worker = setupWorker(
   ...handlers,
   ...workspaceHandlers,
+  ...boardHandlers,
   ...itemHandlers,
   ...cardHandlers,
 );
