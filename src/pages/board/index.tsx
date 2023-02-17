0a81b7a5e5629e4d066d01897e6fb1b068ff7792
seyiul <iseuli713@gmail.com>
2023-02-17 10:55:14 +0900

    style: 주석삭제

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -111,7 +111,6 @@ export default function Board() {
       .catch((error) => alert(error));
   };
 
-  // if (loading) return <BoardSkeleton></BoardSkeleton>;
   if (error)
     return (
       <Inform message="알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요!"></Inform>
