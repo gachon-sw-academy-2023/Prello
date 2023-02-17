eeea354ee12b2dc87f96f1cfee19db8e0d4336c6
Cho Si Yeon <tldusdk5@naver.com>
2023-02-17 11:03:31 +0900

    fix: 한번에 전체 테스트 가능하도록 오류 해결

--- src/tests/cypress/integration/App/deleteBoard.cy.ts
+++ src/tests/cypress/integration/App/deleteBoard.cy.ts
@@ -8,15 +8,23 @@ describe('보드 삭제 테스트', () => {
     cy.wait(3000);
     cy.get('div').contains('워크스페이스 생성').click();
   });
-  it('3. 특정 보드 삭제', () => {
+  it('3. 보드 생성 버튼 클릭', () => {
     cy.wait(1000);
-    cy.get('div[data-testid="update-board"]').click();
+    cy.get('div[data-testid="create-board"]').click();
+
+    cy.wait(500);
+    cy.get('input[data-testid="create-board-name"]').type('보드 생성');
+    cy.get('div').contains('확인').click();
+  });
+  it('4. 특정 보드 삭제', () => {
+    cy.wait(1000);
+    cy.get('div[data-testid="update-board"]').click({ multiple: true });
     cy.get('div').contains('보드 삭제').click();
   });
-  it('4. 워크스페이스 페이지 접속', () => {
+  it('5. 워크스페이스 페이지 접속', () => {
     cy.visit('/workspace');
   });
-  it('5. 특정 워크스페이스 접속', () => {
+  it('6. 특정 워크스페이스 접속', () => {
     cy.wait(3000);
     cy.get('div').contains('워크스페이스 생성').click();
   });
